2cb0eaf146a820b4d0b648b088dfe8b08566fc2b
Peter Pšenák <peter.psenak@naytrolabs.com>
2018-09-27 16:15:52 +0200

    Project done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,30 @@
 // Check off todos by clicking
-$("li").on("click", function(){
-    $(this).toggleClass("completed")
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
 });
 
 // Click on X to delete todo
-$("span").on("click", function(){
-    alert("clicked on span")
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(function(){
+        500,
+        $(this).remove()
+    });
+    e.stopPropagation();
+})
+
+// Input stuff
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13) {
+        // Taking new to-do from input
+        var todoText = $(this).val()
+        $(this).val("")
+        // Create new Aly
+        $("ul")
+            .append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
+    }
+});
+
+// Plus show hide input
+$("#plus").on("click", function(){
+    $("input[type='text']").fadeToggle()
 })
